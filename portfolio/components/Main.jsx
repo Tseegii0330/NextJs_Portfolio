a7fb1961425c31f8a427fd8f9dc7ab0b37e3b754
Mungunshagai <n.tseegii0330@gmail.com>
2023-04-14 13:50:23 +0800

    fix

--- portfolio/components/Main.jsx
+++ portfolio/components/Main.jsx
@@ -1,20 +1,21 @@
 import Image from "next/image";
 import React from "react";
 import { AiOutlineArrowDown } from "react-icons/ai";
-import { MdWavingHand } from "react-icons/md";
 
 export const Main = () => {
   return (
     <div className="w-100 h-screen flex justify-center items-center">
       <div className="max-w-[1240px] w-full h-screen p-2 flex flex-col md:justify-around items-center md:flex-row-reverse ">
-        <div className="py-10">
-          <Image
-            className="rounded-full relative my-10"
-            src="/../public/assets/myimages/me.jpg"
-            alt="/"
-            width={"300"}
-            height={"300"}
-          />
+        <div className="py-10 relative">
+          <div className="before:block before:absolute before:top-20 before:-inset-1 before:h-[300px] before:w-[300px] before:rounded-lg before:-skew-y-2 before:bg-pink-500">
+            <Image
+              className="rounded-full relative my-10"
+              src="/../public/assets/myimages/me.jpg"
+              alt="/"
+              width={"300"}
+              height={"300"}
+            />
+          </div>
         </div>
         <div className="my-0 flex items-center justify-center flex-col md:items-start">
           <p className="uppercase">Let`s build something together</p>
